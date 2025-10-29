c6ec6d02349b25cec6d38a409a17d4d2b94ca28e
Leonardo Aragão <lewoaragao@gmail.com>
2025-10-29 11:50:15 -0300

    Initial commit

---
+++ src/test/java/api/rest/dio/santander/ApiRestDioSantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package api.rest.dio.santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDioSantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
